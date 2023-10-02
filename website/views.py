3d0ecc2a8ed3056903a3d1d1d3fd69487101da6f
Sultanov Aziz <aziz89131983189@gmail.com>
2023-10-02 20:27:42 +0700

    Add urls in our package website and template for several pages

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+def home(request):
+    return render(request, 'home.html', {})
 
-# Create your views here.
+def logout_user(request):
+    pass
